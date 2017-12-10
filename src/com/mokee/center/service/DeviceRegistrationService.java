b3d1ac9d0ac77498800e817dbefe132e16eeb13e
martincz <martincz@mokeedev.com>
2017-12-10 15:17:39 +0800

    fix device registration
    Change-Id: Ie947426ef4c2c8bbfbe5fb09760d85f4d15b336e

--- src/com/mokee/center/service/DeviceRegistrationService.java
+++ src/com/mokee/center/service/DeviceRegistrationService.java
@@ -20,6 +20,9 @@ package com.mokee.center.service;
 import android.app.IntentService;
 import android.content.Intent;
 import com.mokee.os.Build;
+import com.mokee.utils.CommonUtils;
+
+import mokee.providers.MKSettings;
 
 public class DeviceRegistrationService extends IntentService {
 
@@ -29,7 +32,11 @@ public class DeviceRegistrationService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Build.setUniqueIDS(getApplicationContext());
+        if (!CommonUtils.hasTelephony(getApplicationContext())) {
+            MKSettings.Secure.putString(getContentResolver(), MKSettings.Secure.UNIQUE_REGISTRATION_IDS, Build.getUniqueID(getApplicationContext()));
+        } else {
+            MKSettings.Secure.putString(getContentResolver(), MKSettings.Secure.UNIQUE_REGISTRATION_IDS, Build.getUniqueIDS(getApplicationContext()));
+        }
         stopSelf();
     }
 }
